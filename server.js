6c8d4a56e4cdd5de2cc3091ed2e6662fd59b1d70
John Sanders <jws121295@gmail.com>
2015-07-13 22:31:08 -0400

    Server running, but not serving :P

---
+++ server.js
@@ -0,0 +1,43 @@
+/*jshint node: true*/
+var http = require("http");
+var fs = require("fs");
+
+var mainPage = fs.readFileSync(__dirname + "/index.html");
+
+// Expand this section to handle dynamic content
+var slides = fs.readdirSync(__dirname + "/slides");
+var slideIndex = 0;
+//But for now just give a static listing generated at server startup
+function sendSlide(req, res) {
+    var path = __dirname + "/slides/" + slides[slideIndex];
+    fs.createReadStream(path).pipe(res);
+    slideIndex++;
+    if (slideIndex >= slides.length)
+        slideIndex = 0;
+}
+
+http.createServer(function (req, res) {
+    if (req.url === "/") {
+        //Send main page
+        res.writeHead(200);
+        res.write(mainPage);
+        res.end();
+    } else {
+        var get = req.url.match(/^\/get.*$/mgi);
+        if (get) {
+            // Note: no protection against empty directories
+            if (slides[slideIndex]) {
+                res.writeHead(200);
+                sendSlide(req, res);
+                res.end();
+            } else {
+                res.writeHead(404);
+                res.end();
+            }
+        } else {
+            res.writeHead(404);
+            res.end();
+        }
+    }
+
+}).listen(81);
