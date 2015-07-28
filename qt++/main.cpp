27961b3b8e8b68e811ea988f00bbedb380142622
John Sanders <jws121295@gmail.com>
2015-07-28 02:15:46 -0400

    Attached an opacity effect!

--- qt++/main.cpp
+++ qt++/main.cpp
@@ -7,20 +7,14 @@ int main(int argc, char *argv[ ])
 	QApplication app(argc, argv);
     QWidget *window = new QWidget;
 
-    //Working
-//    QGraphicsScene scene;
-//    QPixmap imgA;
-//    imgA.load("slides/download.jpg");
-//    scene.addPixmap(imgA);
-
-    //Not exactly working, preserved
     QGraphicsScene scene;
     QGraphicsPixmapItem item;
     QPixmap img;
     img.load("slides/download.jpg");
     item.setPixmap(img);
+    QGraphicsOpacityEffect opef;
+    item.setGraphicsEffect(&opef);
     scene.addItem(&item);
-
     QGraphicsView view(&scene, window);
 
     window->showFullScreen();
