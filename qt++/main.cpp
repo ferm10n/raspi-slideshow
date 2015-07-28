29505100649527ed4ac1db4dd06d66d56ebe6b7e
John Sanders <jws121295@gmail.com>
2015-07-28 02:39:29 -0400

    ANIMATION!!! and Black background

--- qt++/main.cpp
+++ qt++/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[ ])
 {
 	QApplication app(argc, argv);
     QWidget *window = new QWidget;
+    window->setStyleSheet("QWidget{background-color: #000000;}");
 
     QGraphicsScene scene;
     QGraphicsPixmapItem item;
@@ -13,7 +14,11 @@ int main(int argc, char *argv[ ])
     img.load("slides/download.jpg");
     item.setPixmap(img);
     QGraphicsOpacityEffect *opef = new QGraphicsOpacityEffect;
-    opef->setOpacity(0.1);
+    QPropertyAnimation *anim = new QPropertyAnimation(opef,"opacity");
+    anim->setDuration(1000);
+    anim->setStartValue(1);
+    anim->setEndValue(0);
+    anim->start();
     item.setGraphicsEffect(opef);
     scene.addItem(&item);
     QGraphicsView view(&scene, window);
